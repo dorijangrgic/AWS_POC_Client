f20440bec71358e5889685c18ed3a441bbd7f4d4
Dorijan Grgic <dorijan.grgic@tacta.io>
2023-04-20 16:16:38 +0200

    last fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,7 @@
-import { GetStaticProps } from "next";
-import { Content, StrapiResponse } from "../models/strapi";
-import { About } from "../models/common";
-
-export const getStaticProps: GetStaticProps = async () => {
-  const about: StrapiResponse = await fetch(
-    `http://127.0.0.1:1337/api/about`
-  ).then((res) => res.json());
-
-  const aboutAttributes = (about.data as Content).attributes as About;
-
-  return {
-    props: {
-      aboutAttributes,
-    },
-  };
-};
-
-export default function AboutPage({aboutAttributes}: {aboutAttributes: About}) {
+export default function AboutPage() {
   return (
-    <div dangerouslySetInnerHTML={{ __html: aboutAttributes.about }}></div>
+    <div>
+      <p>Dashboard works!</p>
+    </div>
   );
 }
